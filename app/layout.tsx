62ed99a12b16a7b180f8c09c24de47078e5cf501
Vaishnav Ghenge <vaishnav.ghenge@noovosoft.com>
2024-11-11 15:35:52 +0530

    feat(analytics): add vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type {Metadata, Viewport} from "next";
+import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {SpeedInsights} from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -28,7 +29,8 @@ export default function RootLayout({
     return (
         <html lang='en'>
         <body className={inter.className}>
-        <SpeedInsights/>
+        <SpeedInsights />
+        <Analytics />
         <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0'>
             <div className='lg:flex lg:justify-between lg:gap-4'>
                 <header
