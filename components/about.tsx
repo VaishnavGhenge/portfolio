af8c6f8b7f5dbfc392a7162b9785f60141301a55
Vaishnav Ghenge <vaishnav.ghenge@noovosoft.com>
2025-03-13 00:18:26 +0530

    fix: build

--- components/about.tsx
+++ components/about.tsx
@@ -4,15 +4,15 @@ export default function About() {
             <h2 className='text-lg font-bold uppercase mb-4'>About</h2>
             <div>
                 <p>Hey there! My tech journey kicked off in 2018 when I chose Computer Engineering for my Diploma. I
-                    wrapped up my undergrad in Computer Engineering in July 2024, and since then, I've been working
+                    wrapped up my undergrad in Computer Engineering in July 2024, and since then, I&apos;ve been working
                     full-time at Noovosoft.</p>
                 <p className='mt-4'>I first joined Noovosoft as an intern in August 2023, and after a year of hands-on
-                    experience, I transitioned into a full-time role in August 2024. These days, you’ll find me tackling
-                    exciting projects for Noovosoft’s clients, constantly learning, and leveling up my skills. 🚀</p>
-                <p className='mt-4'>But hey, life’s not just about code! When I’m not deep in development, you’ll
+                    experience, I transitioned into a full-time role in August 2024. These days, you&apos;ll find me tackling
+                    exciting projects for Noovosoft&apos;s clients, constantly learning, and leveling up my skills. 🚀</p>
+                <p className='mt-4'>But hey, life&apos;s not just about code! When I&apos;m not deep in development, you&apos;ll
                     probably find me on the cricket field or hanging out with friends. Because balance is key,
                     right?</p>
-                <p className='mt-4'>So, here’s to a mix of tech, learning, and a bit of cricket. Cheers! 🌟</p>
+                <p className='mt-4'>So, here&apos;s to a mix of tech, learning, and a bit of cricket. Cheers! 🌟</p>
             </div>
         </div>
     )
